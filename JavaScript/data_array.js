f52ac7a84d6a5dd2908f25a819514e0fb395344a
song <44656896+yjxsfyqzysxdysz@users.noreply.github.com>
2019-08-03 10:53:50 +0800

    22

---
+++ JavaScript/data_array.js
@@ -0,0 +1,46 @@
+const arr1 = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 }
+]
+const arr2 = [
+  { id: 6 },
+  { id: 1 },
+  { id: 8 }
+]
+const arr3 = [
+  { id: 1 },
+  { id: 2 },
+  { id: 2 },
+  { id: 3 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 6 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 }
+]
+const arr4 = [1, 2, 3]
+const arr5 = [1, 2, 3, 1, 2, 4, 5, 6]
+const arr6 = [1, 2, 3, 1, 2, 4, 5, 6, 1, '1', '1']
+const arr7 = ['foo', 'book']
+const arr8 = ['a', 'b', 'c']
+
+
+module.exports = {
+  arr1,
+  arr2,
+  arr3,
+  arr4,
+  arr5,
+  arr6,
+  arr7,
+  arr8
+}
