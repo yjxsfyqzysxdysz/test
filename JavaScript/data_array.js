165cae164f89b961d8e54717805acf09ad4ffff9
s <s@163.com>
2019-08-13 12:26:20 +0800

    0

--- JavaScript/data_array.js
+++ JavaScript/data_array.js
@@ -1,38 +1,126 @@
-const arr1 = [
-  { id: 1, res: { 'a': 1 } },
-  { id: 2, res: { 'a': 1 } },
-  { id: 3, res: { 'a': 1 } },
-  { id: 4, res: { 'a': 1 } },
-  { id: 5, res: { 'a': 1 } },
-  { id: 6, res: { 'a': 1 } },
-  { id: 7, res: { 'a': 1 } },
-  { id: 8, res: { 'a': 1 } }
+const arr1 = [{
+    id: 1,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 2,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 3,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 4,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 5,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 6,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 7,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 8,
+    res: {
+      'a': 1
+    }
+  }
 ]
-const arr2 = [
-  { id: 6, res: { 'a': 1 } },
-  { id: 1, res: { 'a': 2 } },
-  { id: 8, res: { 'a': 1 } }
+const arr2 = [{
+    id: 6,
+    res: {
+      'a': 1
+    }
+  },
+  {
+    id: 1,
+    res: {
+      'a': 2
+    }
+  },
+  {
+    id: 8,
+    res: {
+      'a': 1
+    }
+  }
 ]
-const arr3 = [
-  { id: 1 },
-  { id: 2 },
-  { id: 2 },
-  { id: 3 },
-  { id: 3 },
-  { id: 4 },
-  { id: 5 },
-  { id: 6 },
-  { id: 6 },
-  { id: 6 },
-  { id: 7 },
-  { id: 8 }
+const arr3 = [{
+    id: 1
+  },
+  {
+    id: 2
+  },
+  {
+    id: 2
+  },
+  {
+    id: 3
+  },
+  {
+    id: 3
+  },
+  {
+    id: 4
+  },
+  {
+    id: 5
+  },
+  {
+    id: 6
+  },
+  {
+    id: 6
+  },
+  {
+    id: 6
+  },
+  {
+    id: 7
+  },
+  {
+    id: 8
+  }
 ]
 const arr4 = [1, 2, 3]
 const arr5 = [1, 2, 3, 1, 2, 4, 5, 6]
 const arr6 = [1, 2, 3, 1, 2, 4, 5, 6, 1, '1', '1']
 const arr7 = ['foo', 'book']
 const arr8 = ['a', 'b', 'c']
-
+const arr9 = [{
+    startTime: 1565136000000,
+    endTime: 1565141952000
+  },
+  {
+    startTime: 1565142045000,
+    endTime: 1565143200000
+  },
+  {
+    startTime: 1565136000000,
+    endTime: 1565141952000
+  },
+]
 
 module.exports = {
   arr1,
@@ -42,5 +130,6 @@ module.exports = {
   arr5,
   arr6,
   arr7,
-  arr8
+  arr8,
+  arr9
 }
