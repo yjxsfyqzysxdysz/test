35d257e16c680fe6c4389426c07c0037101474de
sxs <sxs@163.com>
2019-11-09 09:03:01 +0800

    0

---
+++ TypeScript/03/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// 1、ts中的累
+var Person = /** @class */ (function () {
+    function Person(n) {
+        // 构造函数 实例化类的时候触发的方法
+        this.name = n;
+    }
+    Person.prototype.run = function () {
+        console.log(this.name);
+    };
+    return Person;
+}());
+var p = new Person('lalal');
+p.run();
