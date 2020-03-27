a48abf0ba4474af579a2a04190e637a1ce0c2794
song <44656896+yjxsfyqzysxdysz@users.noreply.github.com>
2020-03-27 10:29:12 +0800

    0

--- cli/widget_vue_components/src/components/preview.js
+++ cli/widget_vue_components/src/components/preview.js
@@ -1,27 +1,25 @@
 // import axios from 'axios'
 export const openFlow = async (pluginDom, data) => {
+  console.log('data', data)
   // 本级预览开流
   // {devIp: "192.168.20.36", devPort: 3721, channel: 1, streamType: "main"}
-  // let obj = {
-  //   devIp: data.devIp,
-  //   devPort: data.devPort,
-  //   dport: data.dport,
-  //   channel: data.channel,
-  //   streamType: data.streamType || 'main'
-  // }
-  // let res
-  // try {
-  //   res = await axios.post(`ctl/video?type=video&channelid=${data.resId}`, obj)
-  // } catch (err) {
-  //   return err
-  // }
-  // if (res.status !== 200) { return '-1' }
-
+  let obj = {
+    devIp: data.devIp,
+    devPort: data.devPort,
+    dport: data.dport,
+    channel: data.channel,
+    streamType: data.streamType || 'main'
+  }
   // 给插件传参
+  // let params = {
+  //   port: "6003",
+  //   ip: "192.168.20.7",
+  //   cmdStr: "{\"streamType\":\"main\",\"devIp\":\"192.168.20.42\",\"channel\":1,\"devPort\":\"3721\"}"
+  // }
   let params = {
     port: "6003",
     ip: "192.168.20.7",
-    cmdStr: "{\"streamType\":\"main\",\"devIp\":\"192.168.20.42\",\"channel\":1,\"devPort\":\"3721\"}"
+    cmdStr: JSON.stringify(obj)
   }
   return pluginDom.OpenRealStreamEx(JSON.stringify(params))
 }
