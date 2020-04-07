dbf5d42d9de2d13217e753b2b0ebf25936c569e7
s <s@163.com>
2020-04-07 20:55:07 +0800

    ts

--- TypeScript/client-side/src/ts-modules/a.ts
+++ TypeScript/client-side/src/ts-modules/a.ts
@@ -1,21 +1,14 @@
-// 模块和命名空间
-// 模块
-// export
-// import
-// export default
-// export = 和 import xx = require()
+// ts1.5之前有2个概念：内部模块、外部模块
+// 1.5之后，内部模块-》命名空间，外部模块-》模块
+// ts和es6保持一致，包含顶级的import和export文件都被当成一个模块
+// 里面定义的内容仅模块内可见，而不是全局可见
 
-// 命名空间
-// 定义和使用
-// 拆分为多个文件
-
-// 别名
-
-// 模块解析
-// 相对和非相对模块导入
-// 模块解析策略
-// 模块解析配置项
-
-
-
-
+// ts除es6标准的语法外还有一些特定的语法
+// ts 的 export 可导出变量、函数、类，还可导出接口、类型别名
+export interface FuncInterface {
+  name: string
+  (arg: number): string
+}
+export class ClassC {
+  constructor() { }
+}
