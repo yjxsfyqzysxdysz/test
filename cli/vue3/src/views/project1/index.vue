2c627fb0ac6398ebea949d7955ab96e754606eec
song <hellsxs@163.com>
2020-09-22 11:34:35 +0800

    添加新页面

---
+++ cli/vue3/src/views/project1/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="project1">
+    project1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'project1',
+  created() {
+    console.log('/project1')
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="less" scope>
+.project1 {
+}
+</style>
---
+++ cli/vue3/src/views/project1/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="project1Main">
+    main
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'project1Main',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="less" scope>
+.project1Main {
+}
+</style>
