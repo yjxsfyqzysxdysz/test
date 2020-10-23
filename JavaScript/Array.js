4524e8376cb8be649508276a8df5f59bede354f6
song <hellsxs@163.com>
2020-10-23 12:23:25 +0800

    update

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -105,7 +105,7 @@ let aaa = { b: [] }
 aaa.b.map(e => {
   console.log('map', e)
 })
-console.log('aaa')
+// console.log('aaa')
 
 // arr = new Array(3).fill(1) // 索引数组
 // arr = []
@@ -117,3 +117,251 @@ res = arr.join()
 // arr.fill(4)
 // console.log(arr, res, res.split(''))
 // console.log(typeof(true))
+
+// 数组扁平化的几种方法
+/**
+ * Array.prototype.flat()
+ * flat()方法会按照一个可指定的深度depth递归遍历数组，并将所有元素与遍历到的子数组中的元素合并为一个新数组返回。该方法不会改变原数组
+ * ECMAScript 2019
+ * 语法： let newArray = arr.flat(depth)
+ * 描述： depth为指定要提取嵌套数组的结构深度，默认值为 1
+ * flat() 方法会移除数组中的空项
+ */
+
+function Arrayflat(arr = [], num) {
+  // console.log(arr.flat(num))
+}
+Arrayflat([1, 2, [3, 4]]) // [1, 2, 3, 4]
+// 指定要提取嵌套数组的结构深度为1层
+Arrayflat([1, 2, [3, 4, [5, 6]]], 1) // [1, 2, 3, 4, [5, 6]]
+// 指定要提取嵌套数组的结构深度为2层
+Arrayflat([1, 2, [3, 4, [5, 6]]], 2) // [1, 2, 3, 4, 5, 6]
+// 使用 Infinity 作为深度，展开任意深度的嵌套数组
+Arrayflat([1, 2, [3, 4, [5, 6]]], Infinity) // [1, 2, 3, 4, 5, 6]
+// 移除数组中的空项
+Arrayflat([1, 2, , 4, 5]) // [1, 2, 4, 5]
+
+/**
+ * 归并方法 reduce()
+ * 我们用reduce函数进行遍历，把prev的初值赋值为[]，如果当前的值是数组的话，那么我们就递归遍历它的孩子，如果当前的值不是数组，那么我们就把它拼接进数组里。
+ */
+
+arr = [
+  [1, 2, [3, 4], 5],
+  [6, 7, 8],
+  [[9, 10], 11]
+]
+function flat1(arr) {
+  return arr.reduce(function (prev, cur) {
+    return prev.concat(Array.isArray(cur) ? flat1(cur) : cur)
+  }, [])
+}
+// console.log(flat1(arr)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+/**
+ * toString()
+ * 方法有很大局限性，只适用于数组元素全部为数字的情况下
+ *
+ */
+
+// toString && Json.parase
+function flat2(arr) {
+  var str = arr.toString()
+  return JSON.parse('[' + str + ']')
+}
+// console.log(flat2(arr)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+// toString && split
+function flat3(arr) {
+  return arr
+    .toString()
+    .split(',')
+    .map(item => {
+      return Number(item)
+    })
+}
+// console.log(flat3(arr)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+// join && split
+function flat4(arr) {
+  return arr
+    .join(',')
+    .split(',')
+    .map(item => {
+      return Number(item)
+    })
+}
+// console.log(flat4(arr)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+/**
+ * 循环+递归
+ * 递归的遍历每一项，若为数组则继续遍历，否则concat
+ */
+function flat5(arr) {
+  let result = []
+  arr.map(item => {
+    if (Array.isArray(item)) {
+      result = result.concat(flat5(item))
+    } else {
+      result.push(item)
+    }
+  })
+  return result
+}
+// console.log(flat5(arr)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+/**
+ * 扩展运算符. . .
+ * 用于取出参数对象的所有可遍历属性，拷贝到当前对象中
+ */
+function flat6(arr) {
+  while (arr.some(item => Array.isArray(item))) {
+    arr = [].concat(...arr)
+  }
+  return arr
+}
+// console.log(flat6(arr)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+function flat7(arr) {
+  function isFlattenable(value) {
+    const spreadableSymbol = Symbol ? Symbol.isConcatSpreadable : undefined
+    return Array.isArray(value) || !!(spreadableSymbol && value && value[spreadableSymbol])
+  }
+  function flatten(arr, depth, res = []) {
+    let index = -1,
+      len = arr.length
+    while (++index < len) {
+      let value = arr[index]
+      // 校验能否展开
+      // 是否为数组，是否有值，能否展开
+      if (isFlattenable(value)) {
+        if (depth > 1) {
+          flatten(value, depth - 1, res)
+        } else {
+          res = res.concat(value)
+        }
+      } else {
+        res[res.length] = value
+      }
+    }
+    return res
+  }
+  return Array.isArray(arr) && arr.length ? flatten(arr, 1 / 0) : []
+}
+
+const tree = {
+  name: 'root',
+  children: [
+    {
+      name: 'c1',
+      children: [
+        { name: 'c11', children: [] },
+        { name: 'c12', children: [] }
+      ]
+    },
+    {
+      name: 'c2',
+      children: [
+        { name: 'c21', children: [] },
+        { name: 'c22', children: [] }
+      ]
+    }
+  ]
+}
+
+const Stackfn = function () {
+  this.arr = []
+  this.size = function () {
+    return this.arr.length
+  }
+  this.get = function () {
+    return this.arr
+  }
+  this.push = function (val) {
+    if (Array.isArray(val)) {
+      this.arr.push(...val)
+    } else if (val || (!val && isFinite(val))) {
+      this.arr.push(val)
+    }
+    return this.size()
+  }
+  this.pop = function () {
+    return this.arr.pop()
+  }
+  this.clear = function () {
+    this.arr = []
+    return this.size()
+  }
+}
+
+function deep(data) {
+  const stack = new Stackfn()
+  let res = []
+  if (!data) return []
+  stack.push(data)
+  while (stack.size()) {
+    let node = stack.pop()
+    if (!node) continue
+    res.push(node.name)
+    stack.push(node.children.reverse())
+  }
+  return res
+}
+
+// console.log(deep(tree))
+
+// 链表的相加问题
+// 1-2-3 --- 321
+// 4-5-6 --- 654
+// 1-2-3 + 4-5-6 = 5-7-9
+const Node = function (e) {
+  this.ele = e
+  this.next = null
+}
+const LineList = function () {
+  let item = null
+  let len = 0
+  this.append = function (val) {
+    let node = new Node(val)
+    if (!item) {
+      item = node
+    } else {
+      let current = item
+      while (current.next) {
+        current = current.next
+      }
+      current.next = node
+      current = null
+    }
+    node = null
+    len++
+  }
+  this.get = function () {
+    return item
+  }
+}
+function addline(l1, l2) {
+  console.log(l1, l2)
+  let l3 = new LineList(),
+    val = 0
+  while (l1 || l2) {
+    let l1val = l1 ? l1.ele : 0
+    let l2val = l2 ? l2.ele : 0
+    let sum = l1val + l2val + val
+    l3.append(sum % 10)
+    val = Math.floor(sum / 10)
+    if (l1) l1 = l1.next
+    if (l2) l2 = l2.next
+  }
+  if (val) {
+    l3.append(val)
+  }
+  let l3val = l3.get()
+  return l3val
+}
+const l1 = new LineList()
+const l2 = new LineList()
+;(321 + '').split('').forEach(e => l1.append(+e))
+;(654 + '').split('').forEach(e => l2.append(+e))
+// console.log(addline(l1.get(), l2.get()))
+
---
+++ JavaScript/extend.js
@@ -0,0 +1,153 @@
+// 继承
+function Animal() {
+  this.species = '动物'
+}
+
+function Cat(name, color) {
+  this.name = name
+  this.color = color
+}
+let cat
+
+// 构造函数实现"继承"------------------------------------------------
+
+// 1. 构造函数绑定
+// 使用call或apply方法，将父对象的构造函数绑定在子对象上
+// function Cat1(name, color) {
+//   Animal.apply(this, arguments)
+//   this.name = name
+//   this.color = color
+// }
+// cat = new Cat1('大毛', '黄色')
+// console.log(cat.species)
+
+// 2.prototype模式
+// 删除prototype对象原先的值，然后赋予一个新值
+// Cat.prototype = new Animal()
+// 每个prototype都有一个constructor属性，指向其构造函数
+// 由于上式的prototype赋值，将其constructor指向Animal
+// 需将其改回Cat的构造函数
+// Cat.prototype.constructor = Cat
+// 每一个实例上也有一个constructor，默认调用prototype.constructor
+// cat = new Cat('大毛', '黄色')
+// cat.constructor === Cat.prototype.constructor // true
+// console.log(cat.species)
+
+// 如果替换了prototype对象
+// 那下一步必然是为新的prototype对象加上constructor属性
+// 并将这个属性指回原来的构造函数
+
+// 3.直接继承prototype
+// 为上一个方法的改进
+// function Animal1() {}
+// Animal1.prototype.species = '动物'
+// Cat.prototype = Animal1.prototype
+// Cat.prototype.constructor = Cat
+// cat = new Cat('大毛', '黄色')
+// 对比
+// 有点：效率高，不用创建实例，比较省内存
+// 缺点：Cat.prototype和Animal1.prototype指向同一个对象，对Cat.prototype的任何修改都会反映到Animal1.prototype中
+// 所以，由于 Cat.prototype.constructor = Cat
+// 导致 Animal1.prototype.constructor = Cat
+
+// 4.利用空对象作为中介
+// 第3种方法升级
+// function Animal1() {}
+// Animal1.prototype.species = '动物'
+// const F = function () {}
+// F.prototype = Animal1.prototype
+// Cat.prototype = new F()
+// Cat.prototype.constructor = Cat
+// cat = new Cat('大毛', '黄色')
+// console.log(cat.species)
+// F为空对象，几乎不占用内存
+// 修改Cat的prototype对象，就不会影响到Animal.prototype
+
+// 封装继承方法
+// function extend(Child, Parent) {
+//   const F = function () {}
+//   F.prototype = Parent.prototype
+//   Child.prototype = new F()
+//   Child.prototype.constructor = Child
+//   // 属性直接指向父对象的prototype属性。（uber是一个德语词，意思是"向上"、"上一层"。）
+//   // 这等于在子对象上打开一条通道，可以直接调用父对象的方法。
+//   // 这一行放在这里，只是为了实现继承的完备性，纯属备用性质。
+//   Child.prototype.uber = Parent.prototype
+// }
+// extend(Cat, Animal1)
+// cat = new Cat('大毛', '黄色')
+// console.log(cat)
+
+// 5.拷贝继承
+// 采用“拷贝”的方式实现继承
+// 若将父对象的所有属性和方法，拷贝进了子对象，也可以实现继承
+// function Animal1() {}
+// Animal1.prototype.species = '动物'
+// function extend2(Child, Parent) {
+//   let p = Parent.prototype
+//   let c = Child.prototype
+//   for (const k in p) {
+//     c[k] = p[k]
+//   }
+//   c.uber = p
+// }
+
+// extend2(Cat, Animal1)
+// cat = new Cat('大毛', '黄色')
+// console.log(cat.species)
+
+// 非构造函数实现"继承"------------------------------------------------
+
+const Chinese = {
+  nation: '中国',
+  birthPlaces: ['北京', '上海', '香港']
+}
+
+const Doctor = {
+  career: '医生'
+}
+
+// 普通对象间的继承
+
+// 1.object()方法
+// function object(o) {
+//   function F() {}
+//   F.prototype = o
+//   return new F()
+// }
+// 将子对象的prototype属性，指向父对象，从而使得子对象与父对象连在一起。
+// cat = object(Chinese)
+// cat.career = '医生'
+// console.log(cat)
+
+// 2.浅拷贝方式
+// 将父对象的属性全部拷贝给子对象，实现继承
+// function extendCopy(p) {
+//   let c = {}
+//   for (let k in p) {
+//     c[k] = p[k]
+//   }
+//   c.uber = p
+//   return c
+// }
+// cat = extendCopy(Chinese)
+// cat.career = 'doctor'
+// console.log(cat)
+
+// 3.深拷贝
+// function deepCopy(p, c = {}) {
+//   // var c = c || {}
+//   for (const k in p) {
+//     if (typeof p[k] === 'object') {
+//       c[k] = p[k].constructor === Array ? [] : {}
+//       deepCopy(p[k], c[k])
+//     } else {
+//       c[k] = p[k]
+//     }
+//   }
+//   return c
+// }
+// cat = deepCopy(Chinese)
+// Chinese.birthPlaces = ['北京', '上海', '香港']
+// cat.birthPlaces.push('xiamen')
+
