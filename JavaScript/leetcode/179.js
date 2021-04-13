257386817c9c1e1abf75afd59f8709e079d7016e
song <hellsxs@163.com>
2021-04-13 23:17:17 +0800

    largestNumber

--- JavaScript/leetcode/179.js
+++ JavaScript/leetcode/179.js
@@ -28,9 +28,10 @@
  * @return {string}
  */
 var largestNumber = function (nums) {
-  nums.sort((a, b) => '' + b + a - ('' + a + b))
-  if (!nums[0]) return nums[0].toString()
-  return nums.join('')
+  return nums
+    .sort((a, b) => '' + b + a - ('' + a + b))
+    .join('')
+    .replace(/^0+/, '0')
 }
 
-console.log(largestNumber([3, 30, 34, 5, 9]))
+console.log(largestNumber([0]))
