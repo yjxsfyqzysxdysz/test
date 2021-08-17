2aab4398a2a1082e5ade1a274140b20e37e54e80
song <hellsxs@163.com>
2021-08-18 02:02:20 +0800

    update

--- exerciseBook.js
+++ exerciseBook.js
@@ -6,6 +6,40 @@
  * 出参 [[1, ], [2, ]]
  */
 const params = []
-function fun1(params) {}
+function fun1(params) {
+  const obj = {}
+  params.forEach(([id, value]) => {
+    if (!obj.hasOwnProperty(id)) {
+      obj[id] = [value]
+    } else {
+      obj[id].push(value)
+    }
+  })
+  let res = []
+  for (const key in obj) {
+    res.push([
+      +key,
+      Math.floor(
+        obj[key]
+          .sort((a, b) => b - a)
+          .slice(0, 5)
+          .reduce((acc, cur) => acc + cur) / 5
+      )
+    ])
+  }
+  return res
+}
 
 console.log(fun1(params))
+
+/**
+ *
+ */
+function fun2(params) {}
+console.log(fun2(params))
+
+/**
+ * 二叉树
+ */
+function fun3(params) {}
+console.log(fun3(params))
